4a222d06b50cee3a0ca8d44b20a2863814e7ba1f
Yusuf Olokoba <olokobayusuf@gmail.com>
2018-08-26 20:31:27 -0400

    Updated Android saveToCameraRoll to copy media file to gallery

--- Android/NatShare/src/main/java/com/yusufolokoba/natshare/NatShare.java
+++ Android/NatShare/src/main/java/com/yusufolokoba/natshare/NatShare.java
@@ -15,8 +15,10 @@ import android.webkit.MimeTypeMap;
 
 import com.unity3d.player.UnityPlayer;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
 
@@ -76,14 +78,13 @@ public class NatShare {
         return true;
     }
 
-    public static boolean saveImageToCameraRoll (byte[] pngData) { // DEPLOY
-        ContentValues values = new ContentValues(3);
-        values.put(MediaStore.Images.Media.TITLE, "Image");
+    public static boolean saveImageToCameraRoll (byte[] pngData) {
+        final ContentValues values = new ContentValues();
         values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
         values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
         values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
-        ContentResolver resolver = UnityPlayer.currentActivity.getContentResolver();
-        Uri url = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        final ContentResolver resolver = UnityPlayer.currentActivity.getContentResolver();
+        final Uri url = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
         try {
             OutputStream stream = resolver.openOutputStream(url);
             stream.write(pngData);
@@ -100,16 +101,17 @@ public class NatShare {
     public static boolean saveMediaToCameraRoll (String path) {
         File file = new File(path);
         if (!file.exists()) return false;
-        final String mimeType = getMimeType(path);
-        ContentValues values = new ContentValues(3);
-        values.put(MediaStore.Video.Media.TITLE, file.getName());
-        values.put(MediaStore.Video.Media.MIME_TYPE, mimeType);
-        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis());
-        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
-        values.put(MediaStore.Video.Media.DATA, path);
-        if (mimeType.toLowerCase().startsWith("video"))
-            values.put(MediaStore.Video.Media.DURATION, getVideoDuration(path));
-        UnityPlayer.currentActivity.getContentResolver().insert(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, values);
+        // Copy file to gallery folder
+        File galleryDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        File galleryFile = new File(galleryDirectory, file.getName());
+        try {
+            copyFile(file, galleryFile);
+        } catch (IOException ex) {
+            Log.e("Unity", "NatShare Error: Failed to copy media to camera roll");
+            ex.printStackTrace();
+            return false;
+        }
+        UnityPlayer.currentActivity.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(galleryFile)));
         return true;
     }
 
@@ -144,13 +146,18 @@ public class NatShare {
         return type;
     }
 
-    private static long getVideoDuration (String url) {
-        // Source: https://github.com/furmankl/NatShare-API, thank you!
-        MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-        retriever.setDataSource(url);
-        String time = retriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
-        long timeInMillisec = Long.parseLong(time);
-        retriever.release();
-        return timeInMillisec;
+    private static void copyFile (File src, File dst) throws IOException {
+        InputStream is = null;
+        OutputStream os = null;
+        try {
+            is = new FileInputStream(src);
+            os = new FileOutputStream(dst);
+            final byte[] buffer = new byte[1024];
+            int length;
+            while ((length = is.read(buffer)) > 0) os.write(buffer, 0, length);
+        } finally {
+            if (is != null) is.close();
+            if (os != null) os.close();
+        }
     }
 }
