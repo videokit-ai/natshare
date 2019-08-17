3a3f6732a4393b7bf4ae972c88a87db3c3866a6b
Yusuf Olokoba <yusuf@Yusufs-MacBook-Pro.local>
2019-08-17 14:00:04 -0400

    Minor updates

--- Android/NatShare/src/main/java/com/yusufolokoba/natshare/SharePayload.java
+++ Android/NatShare/src/main/java/com/yusufolokoba/natshare/SharePayload.java
@@ -1,15 +1,14 @@
 package com.yusufolokoba.natshare;
 
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Environment;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.support.v4.content.FileProvider;
 import android.util.Log;
 import com.unity3d.player.UnityPlayer;
-import com.unity3d.player.UnityPlayerActivity;
-import com.unity3d.player.UnityPlayerNativeActivity;
 
 import java.io.File;
 import java.io.FileOutputStream;
@@ -23,11 +22,13 @@ import java.util.ArrayList;
  */
 public final class SharePayload implements Payload {
 
+    private final Runnable completionHandler;
     private final Intent intent;
     private final ArrayList<Bitmap> images;
-    private int size;
+    private final ArrayList<Uri> uris;
 
     public SharePayload (String subject, Runnable completionHandler) { // INCOMPLETE
+        this.completionHandler = completionHandler;
         // Create intent
         intent = new Intent();
         intent.putExtra(Intent.EXTRA_SUBJECT, subject);
@@ -35,12 +36,12 @@ public final class SharePayload implements Payload {
         intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         // Create collections
         images = new ArrayList<>();
+        uris = new ArrayList<>();
     }
 
     @Override
     public void addText (String text) {
         intent.putExtra(Intent.EXTRA_TEXT, text);
-        size++;
     }
 
     @Override
@@ -50,6 +51,7 @@ public final class SharePayload implements Payload {
         image.copyPixelsFromBuffer(ByteBuffer.wrap(pixelBuffer));
         images.add(image);
         // Write to file
+        /*
         try {
             File file = new File(UnityPlayer.currentActivity.getExternalCacheDir(), System.nanoTime() + ".png");
             FileOutputStream outputStream = new FileOutputStream(file);
@@ -60,20 +62,49 @@ public final class SharePayload implements Payload {
         } catch (IOException ex) {
             Log.e("Unity", "NatShare Error: Failed to add image to share payload with error: " + ex);
         }
+        */
     }
 
     @Override
     public void addMedia (String uri) { // DEPLOY
-        Uri contentUri = FileProvider.getUriForFile(UnityPlayer.currentActivity, UnityPlayer.currentActivity.getPackageName() + ".fileprovider", new File(uri));
-        intent.putExtra(Intent.EXTRA_STREAM, Uri.parse(uri));
+        Uri contentUri = FileProvider.getUriForFile(UnityPlayer.currentActivity, UnityPlayer.currentActivity.getPackageName() + ".natshare", new File(uri));
+        uris.add(contentUri);
+        Log.d("Unity", "Content URI: "+contentUri);
     }
 
     @Override
     public void commit () { // INCOMPLETE
+        // Create worker thread for IO
+        final HandlerThread commitThread = new HandlerThread("SharePayload Commit Thread");
+        commitThread.start();
+        final Handler commitHandler = new Handler(commitThread.getLooper());
+        final Handler delegateHandler = new Handler(Looper.myLooper());
+        // Commit
+        commitHandler.post(new Runnable() {
+            @Override
+            public void run () {
+                // Write images
+                for (int i = 0; i < images.size(); i++) {
+                    final Bitmap image = images.get(i);
+                    try {
+                        File file = new File(UnityPlayer.currentActivity.getCacheDir(), "natshare." + i + ".png");
+                        FileOutputStream outputStream = new FileOutputStream(file);
+                        image.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+                        outputStream.close();
+                        Uri fileUri = FileProvider.getUriForFile(UnityPlayer.currentActivity, UnityPlayer.currentActivity.getPackageName() + ".natshare", file);
+                        uris.add(fileUri);
+                    } catch (IOException ex) {
+                        Log.e("Unity", "NatShare Error: SharePayload failed to commit image with error: " + ex);
+                    } finally {
+                        image.recycle();
+                    }
+                }
+                images.clear();
+                //
+            }
+        });
+        commitThread.quitSafely();
         // Set action
 
-        // ...
-        for (Bitmap image : images)
-            image.recycle();
     }
 }
