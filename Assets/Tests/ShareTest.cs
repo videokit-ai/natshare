65a59b6a336814b1c93f9e1b083d774968b5a164
Yusuf Olokoba <olokobayusuf@gmail.com>
2021-01-12 15:06:58 -0500

    Add sharing tests

--- Assets/Tests/ShareTest.cs
+++ Assets/Tests/ShareTest.cs
@@ -5,25 +5,116 @@
 
 namespace NatSuite.Tests {
 
-    using UnityEngine;
-    using System.IO;
+    using System;
     using System.Threading.Tasks;
+    using UnityEngine;
     using Sharing;
 
     public class ShareTest : MonoBehaviour {
 
         async void Start () {
             Application.SetStackTraceLogType(LogType.Log, StackTraceLogType.None);
-            await Task.Delay(2000);
-            // Get assets to share
-            var screenshot = ScreenCapture.CaptureScreenshotAsTexture();
-            var basePath = Application.platform == RuntimePlatform.Android ? Application.persistentDataPath : Application.streamingAssetsPath;
-            var videoPath = Path.Combine(basePath, "video.mp4");
-            // Share
+            var tests = new Func<Task>[] {
+                //TestShareText,
+                //TestShareImage,
+                TestShareVideo,
+                //TestShareTextImage,
+                //TestShareTextVideo,
+                //TestShareImageVideo,
+                //TestShareTextImageVideo
+            };
+            foreach (var test in tests) {
+                await Task.Delay(3_000);
+                await test();
+            }
+        }
+
+        async Task TestShareText () {
+            // Payload
+            var text = "Sharing is caring!";
+            // Commit
             var payload = new SharePayload();
-            payload.AddImage(screenshot);
+            payload.AddText(text);
             var success = await payload.Commit();
-            Debug.Log($"Successfully shared items: {success}");
+            Debug.Log($"Successfully shared text: {success}");
+        }
+
+        async Task TestShareImage () {
+            // Payload
+            var image = ScreenCapture.CaptureScreenshotAsTexture();
+            // Commit
+            var payload = new SharePayload();
+            payload.AddImage(image);
+            var success = await payload.Commit();
+            Debug.Log($"Successfully shared image: {success}");
+            // Cleanup
+            Texture2D.Destroy(image);
+        }
+
+        async Task TestShareVideo () {
+            // Payload
+            var videoPath = await Utility.PathFromStreamingAssets("pexels_video.mp4");
+            // Commit
+            var payload = new SharePayload();
+            payload.AddMedia(videoPath);
+            var success = await payload.Commit();
+            Debug.Log($"Successfully shared video: {success}");
+        }
+
+        async Task TestShareTextImage () {
+            // Payload
+            var text = "Sharing is caring!";
+            var image = ScreenCapture.CaptureScreenshotAsTexture();
+            // Commit
+            var payload = new SharePayload();
+            payload.AddText(text);
+            payload.AddImage(image);
+            var success = await payload.Commit();
+            Debug.Log($"Successfully shared text and image: {success}");
+            // Cleanup
+            Texture2D.Destroy(image);
+        }
+
+        async Task TestShareImageVideo () {
+            // Payload
+            var image = ScreenCapture.CaptureScreenshotAsTexture();
+            var videoPath = await Utility.PathFromStreamingAssets("pexels_video.mp4");
+            // Commit
+            var payload = new SharePayload();
+            payload.AddImage(image);
+            payload.AddMedia(videoPath);
+            var success = await payload.Commit();
+            Debug.Log($"Successfully shared image and video: {success}");
+            // Cleanup
+            Texture2D.Destroy(image);
+        }
+
+        async Task TestShareTextVideo () {
+            // Payload
+            var text = "Sharing is caring!";
+            var videoPath = await Utility.PathFromStreamingAssets("pexels_video.mp4");
+            // Commit
+            var payload = new SharePayload();
+            payload.AddText(text);
+            payload.AddMedia(videoPath);
+            var success = await payload.Commit();
+            Debug.Log($"Successfully shared text and video: {success}");
+        }
+
+        async Task TestShareTextImageVideo () {
+            // Payload
+            var text = "Sharing is caring!";
+            var image = ScreenCapture.CaptureScreenshotAsTexture();
+            var videoPath = await Utility.PathFromStreamingAssets("pexels_video.mp4");
+            // Commit
+            var payload = new SharePayload();
+            payload.AddText(text);
+            payload.AddImage(image);
+            payload.AddMedia(videoPath);
+            var success = await payload.Commit();
+            Debug.Log($"Successfully shared image and video: {success}");
+            // Cleanup
+            Texture2D.Destroy(image);
         }
     }
 }
